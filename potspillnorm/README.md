4b4396ddc568e8de81011bbd73d6c662850bf1c2
twongjirad <taritree.wongjirad@gmail.com>
2019-11-12 00:03:56 -0500

    save pot tools

---
+++ potspillnorm/README.md
@@ -0,0 +1,7 @@
+# potspillnorm
+
+Code to calculate POT and number of spills in a sample for normalization purposes.
+
+Provides scripts to interface with Tufts PUBs system and samweb.
+
+Follows prescription from Davio, [doc-db 25475](https://microboone-docdb.fnal.gov/cgi-bin/private/ShowDocument?docid=25475).
---
+++ potspillnorm/get_samname_list.py
@@ -0,0 +1,28 @@
+import os,sys,argparse,json
+
+# name a dataset/table to get files for
+sample="mcc9jan_extbnb"
+#table="dlmergedsparsessnet_mcc9jan_bnb5e19"
+#table="dlmergedsparsessnet_mcc9_v13_bnb_overlay_run1"
+#table="dlmergedsparsessnet_mcc9_v13_nueintrinsics_overlay_run1"
+
+flistfolder="filelists"
+
+query ="SELECT t1.run,t1.subrun,pathtable.superasam"
+query+=" from dlmergedsparsessnet_%s t1 join %s_paths pathtable on (t1.run=pathtable.run and t1.subrun=pathtable.subrun)"%(sample,sample)
+query+=" where status=4 order by t1.run,t1.subrun asc"
+cmdx="echo \"\\x \\\\\\\\ %s\" | psql -h lnsnudot.mit.edu -U tufts-pubs procdb" %( query )
+print cmdx
+pout = os.popen(cmdx)
+plines = pout.readlines()
+
+fout = open('%s/%s.list'%(flistfolder,sample),'w')
+for l in plines:
+    l = l.strip()
+    info = l.split("|")
+
+    if len(info)>1 and "superasam"==info[0].strip():
+        print>>fout,info[1].strip()
+
+fout.close()
+
