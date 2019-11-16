514430848c7f6a7cdbdde00588a43d98ff534a5d
twongjirad <taritree.wongjirad@gmail.com>
2019-11-15 22:35:36 -0500

    completed opflashpe

--- filecheck/plot_opflashpe.py
+++ filecheck/plot_opflashpe.py
@@ -1,21 +1,7 @@
 import os,sys
 import ROOT as rt
 
-rt.gStyle.SetOptStat(0)
-
-samples = ["mcc9jan_extbnb","mcc9jan_bnb5e19","mcc9_v13_bnb_overlay_run1","mcc9_v13_overlay_dirt_run1"]
-scales = { "mcc9jan_extbnb":(9571011+2220.0)/(12824524.0+15585.0),
-           "mcc9_v13_bnb_overlay_run1":4.3e19/(2.0e18*24324/50.0),
-           "mcc9_v13_overlay_dirt_run1":4.3e19/(3.12362163789e+20),
-           "mcc9jan_bnb5e19":1.0 }
-colors = { "mcc9jan_extbnb":rt.kBlue,
-           "mcc9_v13_bnb_overlay_run1":rt.kRed,
-           "mcc9_v13_overlay_dirt_run1":rt.kGreen+2,
-           "mcc9jan_bnb5e19":rt.kBlack }
-fills = { "mcc9jan_extbnb":3003,
-           "mcc9_v13_bnb_overlay_run1":3003,
-           "mcc9_v13_overlay_dirt_run1":3003,
-           "mcc9jan_bnb5e19":0 }
+import utils
 
 # Canvas
 c = rt.TCanvas("copflash","Opflash PE",1000,500)
@@ -29,15 +15,13 @@ maxbin = 30e3
 
 out = rt.TFile("temp.root","recreate")
 # define hits
-for sample in samples:
+for sample in utils.samples:
     hn = hname+"_"+sample
     print hn    
     h = rt.TH1D( hn, "", nbins, minbin, maxbin )
+    utils.set_hist_style( h, sample )
     hists[sample] = h
-    hists[sample].SetLineColor( colors[sample] )
-    hists[sample].SetFillColor( colors[sample] )
-    hists[sample].SetFillStyle( fills[sample] )
-
+    
 out.ls()
 
 
@@ -45,7 +29,7 @@ out.ls()
 tree = "dlsummary"
 rfile = {}
 trees = {}
-for sample in samples:
+for sample in utils.samples:
     fname = "mcc9_v13/summarytree_dlmerged_"+sample+".root"
     rfile[sample] = rt.TFile( fname )
     trees[sample] = rfile[sample].Get(tree)
@@ -53,24 +37,33 @@ for sample in samples:
     out.cd()
     
     hn = hname+"_"+sample
-    trees[sample].Draw("opflashpe>>%s"%(hn),"opflashpe>0.0")
+    trees[sample].Draw("opflashpe>>%s"%(hn))
     prescale = hists[sample].Integral()
-    hists[sample].Scale( scales[sample] )
+    utils.scale_hist( hists[sample], sample )
     print "  hist integral, ",sample,": prescale=",prescale," post-scale=",hists[sample].Integral()
     
 
 # stacked prediction
-hstack = rt.THStack("hstack","")
-hstack.Add( hists["mcc9jan_extbnb"] )
-hstack.Add( hists["mcc9_v13_bnb_overlay_run1"] )
-hstack.Add( hists["mcc9_v13_overlay_dirt_run1"] )
+hstack = utils.make_stack( "hopflashpe_stack", hists )
+
+hmax_stack = hstack.GetMaximum()
+hmax_bnb   = hists["mcc9jan_bnb5e19"].GetMaximum()
+
+hmax = None
+if hmax_stack>hmax_bnb:
+    hmax = hstack
+else:
+    hmax = hists["mcc9jan_bnb5e19"]
 
 # Draw hists
-hstack.Draw("histE0samE")
+hmax.Draw("hist")
+hstack.Draw("histE0same")
 hists["mcc9jan_bnb5e19"].SetLineWidth(2)
 hists["mcc9jan_bnb5e19"].Draw("E1same")
 
 c.Draw()
+c.SetLogy(1)
+#c.SetLogx(1)
 c.Update()
 
 raw_input()
