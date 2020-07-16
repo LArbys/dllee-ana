4f7f62ca877d5a81b6830b50b17fe26cfbf2f45d
jmoon2610 <jarrett@mit.edu>
2020-07-16 09:47:42 -0500

    Update for new BDT & cuts expectation

--- blind-plots/MakeBlindPlots.py
+++ blind-plots/MakeBlindPlots.py
@@ -40,7 +40,7 @@ if APPLY_BQ:
     with open("BeamQualityFilter.pickle","wb") as handle: PassBeamQuality = pickle.load(handle)
 
 TARGET_POT = float(argv[2])
-KEEP_N     = int(8*TARGET_POT / 1.0e20)
+KEEP_N     = int(6*TARGET_POT / 1.0e20)
         
 ChooseMe = {}
 for event in INPUT_TREE:
